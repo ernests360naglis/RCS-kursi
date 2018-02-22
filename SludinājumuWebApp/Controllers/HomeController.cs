abd7020e2a7ff2ef315acd7192f21f9a3c1fd2a3
ernests360naglis <36454603+ernests360naglis@users.noreply.github.com>
2018-02-22 21:25:54 +0200

    SludinājumiWebLapa

--- SludinājumuWebApp/Controllers/HomeController.cs
+++ SludinājumuWebApp/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SludinājumuWebApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,9 +9,50 @@ namespace SludinājumuWebApp.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
+        public HomeController()
+        {
+            adverts = new List<Sludinājums>();
+
+            Sludinājums ad1 = new Sludinājums();
+            ad1.AdvertId = 1;
+            ad1.Name = "BMW";
+            ad1.AdvertText = "Šis ir labs BMW";
+            ad1.Price = 2500;
+            ad1.CreationTime = DateTime.Now;
+
+            Sludinājums homeAd = new Sludinājums();
+            homeAd.AdvertId = 2;
+            homeAd.Name = "Māja";
+            homeAd.AdvertText = "Liela māja";
+            homeAd.Price = 12000;
+            homeAd.CreationTime = new DateTime(1999, 12, 31);
+
+            adverts.Add(ad1);
+            adverts.Add(homeAd);
+        }
+        private List<Sludinājums> adverts;
+        //ši f-cija tiek izsaukta, kad tiek pieprasīta WebLapas bāzes ceļš
+        // GET: Home (piemēram www.ss.lv)
         public ActionResult Index()
         {
+            //izsaucam Viewe f-ciju, lai uzģenerētu html rezultātu no mūsu Index.cshtml faila, 
+            //tajā iekšā izmantojot datus, kas pieejami adverts sarakstā
+            return View(adverts);
+        }
+
+        public ActionResult Advert(int advertId)
+        {
+            //apskatām katru sludinājumu sarakstā
+            foreach(var ad in adverts)
+            {
+                //ja sludinājuma id ir tāds pats ko lietotājs pieprasījis,
+                if(ad.AdvertId == advertId)
+                {
+                    //tad izveidojam skatu izmantojot šī sludinājuma datus un atgriežam lietotājam
+                    return View(ad);
+                }
+            }
+
             return View();
         }
     }
---
+++ SludinājumuWebApp/Models/Sludinājums.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SludinājumuWebApp.Models
+{
+    public class Sludinājums
+    {
+        public int AdvertId { get; set; }
+        public double Price { get; set; }
+        public string Name { get; set; }
+        public string AdvertText { get; set; }
+        public DateTime CreationTime { get; set; }
+    }
+}
